98288338f4fa89776078cafb036023cdf27e5742
weiliu-gis <wliu54@buffalo.edu>
2023-06-20 14:08:14 -0400

    some update

--- global.R
+++ global.R
@@ -34,14 +34,6 @@ crs_latlng <- '+proj=longlat +datum=WGS84' # Latitudes and longitudes for visual
 
 # Load all datasets
 
-# Load the New York State shape file
-nys_shp <- st_read("./data/NYS_State_shp/State_Shoreline.shp")
-nys_shp <- st_transform(nys_shp, crs = crs_latlng)
-
-# Load the NYS counties shp
-nys_counties_shp <- st_read("./data/NYS_Counties_shp/Counties_Shoreline.shp")
-nys_counties_shp <- st_transform(nys_counties_shp, crs = crs_latlng)
-
 # Load the geocoded alcohol dataset (This dataset is created by overlaying the geocoded alcohol shp with NYS counties shp.)
 alcohol_loc_by_county <- read_rds("./data/Alcohol_Locations_exclude_NYC_LongIsland_sf.rds")
 
